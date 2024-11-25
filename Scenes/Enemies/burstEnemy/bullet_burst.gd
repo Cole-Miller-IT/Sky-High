651b8dbc7674b3445a0345189a347fa863a1e07d
Cole-Miller-IT <68202945+Cole-Miller-IT@users.noreply.github.com>
2024-11-25 15:10:25 -0500

    Added bullet burst

--- Scenes/Enemies/burstEnemy/bullet_burst.gd
+++ Scenes/Enemies/burstEnemy/bullet_burst.gd
@@ -1,11 +1,14 @@
 extends Node2D
 class_name BulletBurst
 
+
+var speed : float = 150.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	position += transform.x * speed * delta
--- Scenes/Enemies/burstEnemy/enemy_burst.gd
+++ Scenes/Enemies/burstEnemy/enemy_burst.gd
@@ -4,7 +4,7 @@ class_name BurstEnemy
 var shotDamage = 5
 var shotDamageMultiplier = 1
 var burst = 3
-@export var bullet : PackedScene
+var bullet = preload('res://Scenes/Enemies/burstEnemy/bullet_burst.tscn')
 
 func _physics_process(delta): 
 	var direction = global_position.direction_to(player.global_position)
@@ -18,22 +18,18 @@ func _physics_process(delta):
 
 func attack(value):
 	print("------------enemy attack now-----------")
-	var damagePerShot = value * shotDamageMultiplier
+	#var damagePerShot = value * shotDamageMultiplier
 	
 	for i in range(burst):
-		print("new bullet")
-		#instance a bullet
 		var newBullet = bullet.instantiate()
 		owner.add_child(newBullet)
 		
-		
+		#
 		newBullet.transform = $Muzzle.global_transform
 		
-		#add_child(newBullet)
-		
-		#Assign it a way to die
-
-
+		#wait 0.1 seconds before next shot
+		await get_tree().create_timer(0.1).timeout
+	
 func _on_attack_cooldown_timeout():
 	#shoot a 3 round burst at the player
 	attack(shotDamage)
